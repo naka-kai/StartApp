10f2a9b2f934407be4f33c041651de0f091a9341
kai <sea.k.naka@gmail.com>
2024-06-23 22:35:44 +0900

    Initial Commit

---
+++ StartApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StartApp
+//
+//  Created by 中島叶偉 on 2024/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, SwiftUI")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StartApp/StartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartAppApp.swift
+//  StartApp
+//
+//  Created by 中島叶偉 on 2024/06/23.
+//
+
+import SwiftUI
+
+@main
+struct StartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
