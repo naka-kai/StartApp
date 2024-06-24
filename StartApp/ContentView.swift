0f0d5d093599c3758107e1f23d3341ef3d0f2a24
kai <sea.k.naka@gmail.com>
2024-06-25 08:13:51 +0900

    next commit

--- StartApp/ContentView.swift
+++ StartApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, SwiftUI!")
+            Text("Hello, SwiftUI")
         }
         .padding()
     }
